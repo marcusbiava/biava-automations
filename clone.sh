13fb8eb1710b6bf2d8f9233b1089156e3069ce31
marcusbiava <marcusbiava@gmail.com>
2021-01-18 12:55:54 -0300

    Create clone.sh

---
+++ clone.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd ~/dev/source/ || exit
+git clone git@github.com:marcusbiava/scripts.git
+git clone git@github.com:marcusbiava/jboss.git
+git clone git@github.com:marcusbiava/flyway-docker.git
